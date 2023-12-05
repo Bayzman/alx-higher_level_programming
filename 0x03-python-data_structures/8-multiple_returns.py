45cd113555ac8da80dc32069d702ceb705947be8
bayzman <bassmaninno102@gmail.com>
2023-12-05 14:56:11 -0800

    multiple_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,8 @@ def multiple_returns(sentence):
     if (sentence == ""):
         length = len(sentence)
         first = None
-        print("Length: {} - First character: {}".format(length, first))
+        return (length, first)
 
     length = len(sentence)
     first = sentence[0]
-    print("Length: {} - First character: {}".format(length, first))
+    return(length, first)
