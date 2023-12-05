45837cb67f121d15ac78eb1985a0031cc3e1c606
bayzman <bassmaninno102@gmail.com>
2023-12-05 13:45:46 -0800

    edit folder name

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx < len(my_list)):
-        return my_list[idx]
-    elif (idx < 0 and idx != 0):
+    if (idx < 0):
         return None
+
+    elif (idx < len(my_list)):
+        return my_list[idx]
+
     else:
         return None
