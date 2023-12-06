896bdf02d02466a84df6c115ce5b1cd2b62e27fe
bayzman <bassmaninno102@gmail.com>
2023-12-06 15:47:50 -0800

    squared matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if (matrix=[]):
-        return matrix
+        return
 
     new_matrix = [[0 for _ in range(len(matrix[0]))] \
                   for _ in range(len(matrix))]
