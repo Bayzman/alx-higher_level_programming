8dc378e45bf0e05f5d78b7853a4370db6e74af9f
bayzman <bassmaninno102@gmail.com>
2023-12-06 17:25:33 -0800

    multiply dictionary values by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multipy_by_2(a_dictionary):
-    if (a_dictionary):
-        for key, value in a_dictionary.items():
-            a_dictionary[key] = value * 2
+    for key, value in a_dictionary.items():
+        a_dictionary[key] = value * 2
 
     return a_dictionary
