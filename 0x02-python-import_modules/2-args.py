809dbfa206f3cd678f0df95e2f4ed567c94f5a79
bayzman <bassmaninno102@gmail.com>
2023-12-06 18:04:31 -0800

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    total = len(sys.argv)
-    if total <= 1:
+    length = len(sys.argv)
+    if length <= 1:
         print("0 arguments.")
     else:
-        if total == 2:
-            print("{} argument:".format(total - 1))
+        if length == 2:
+            print(f"{length - 1} argument:")
         else:
-            print("{} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{}: {}".format(i, sys.argv[i]))
+            print(f"{length - 1} arguments:")
+        for i in range(1, length):
+            print(f"{i}: {sys.argv[i]}")
