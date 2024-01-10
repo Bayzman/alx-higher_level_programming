32347741c898692878cb74006546f29a845bca80
bayzman <bassmaninno102@gmail.com>
2024-01-09 17:05:23 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename='') -> None:
     Args:
         filename (str): file name
     '''
-    with open(filename) as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
            print(line, end='')
