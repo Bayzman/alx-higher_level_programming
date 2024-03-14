46b5916cb04a74c9522ebb141fffe7f14f663d9d
Bayzman <bassmaninno102@gmail.com>
2024-03-13 19:22:26 -0700

    javascript warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,8 +5,8 @@ const args = process.argv.slice(2);
 const argSize = Number(args[0]);
 
 if (isNaN(argSize) === false) {
-  for (let i = 0; i < numArg; i++) {
-    console.log('X'.repeat(numArg));
+  for (let i = 0; i < argSize; i++) {
+    console.log('X'.repeat(argSize));
   }
 } else {
   console.log('Missing size');
