b4eea22dff13d2c73902bf614c26b338c0631148
Bayzman <bassmaninno102@gmail.com>
2024-03-13 19:06:01 -0700

    javascript objects, scopes, and closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,16 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const dict = require('./101-data.js').dict;
 
 const userByOccurrences = {};
 
 for (const id in dict) {
-
   const occurrences = dict[id];
 
   if (!userByOccurrences[occurrences]) {
     userByOccurrences[occurrences] = [];
   }
- 
+
   userByOccurrences[occurrences].push(id);
 }
 
