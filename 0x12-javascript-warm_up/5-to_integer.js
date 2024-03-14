68bfff4959cfda4fcd7c76426b46a84b88337e4a
Bayzman <bassmaninno102@gmail.com>
2024-03-13 19:15:57 -0700

    javascript warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 const argSize = Number(args[0]);
 
 if (isNaN(argSize) === false) {
-  console.log(`My number: ${numArg}`);
+  console.log(`My number: ${argSize}`);
 } else {
   console.log('Not a number');
 }
