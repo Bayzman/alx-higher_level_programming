c9975adf9414bfe8f5a3fb7bed2f2209861f4cc4
bayzman <bassmaninno102@gmail.com>
2024-03-28 19:15:33 -0700

    Post an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,7 +3,7 @@
 """ takes in a URL and an email, sends a POST request """
 
 from sys import argv
-import urllib
+from urllib import request, parse
 
 
 def main():
@@ -12,10 +12,10 @@ def main():
     url = argv[1]
     email = argv[2]
     values = {'email': email}
-    content = urllib.parse.urlencode(values)
+    content = parse.urlencode(values)
 
-    request = urllib.request.Request(url, content.encode('ascii'))
-    with urllib.request.urlopen(request) as response:
+    request = request.Request(url, content.encode('ascii'))
+    with request.urlopen(request) as response:
         html = response.read()
         print(html.decode('utf-8'))
 
