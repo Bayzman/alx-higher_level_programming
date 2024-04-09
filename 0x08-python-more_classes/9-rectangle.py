bab4b170d62a26ae751c0e87521d0b5365ea1f55
Bayzman <bassmaninno102@gmail.com>
2024-04-08 19:00:10 -0700

    a square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -5,11 +5,14 @@
 
 class Rectangle():
     """ Defines a rectangle """
+    number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """ Class initialization """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -55,7 +58,7 @@ class Rectangle():
         if (self.__width == 0 or self.__height == 0):
             return ''
         for i in range(self.__height):
-            symbol += ('#' * self.__width)
+            symbol += (str(self.print_symbol) * self.__width)
             if (i != (self.__height - 1)):
                 symbol += '\n'
 
@@ -64,3 +67,29 @@ class Rectangle():
     def __repr__(self):
         """ Returns a string representation of the rectangle """
         return (f'Rectangle({self.__width}, {self.__height})')
+
+    def __del__(self):
+        """ Deletes an instance of the Rectangle class """
+        print('Bye rectangle...')
+        self.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ Returns the bigger rectangle based on the area """
+
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_2 must be an instance of Rectangle')
+
+        if (rect_1.area() >= rect_2.area()):
+            return rect_1
+
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """ Returns a new Rectangle instance with width = height = size """
+        return Rectangle(size, size)
