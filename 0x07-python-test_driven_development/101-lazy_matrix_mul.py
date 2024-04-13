ca70df184775a2a9697e2f7b69ad168a618f204f
Bayzman <bassmaninno102@gmail.com>
2024-04-12 22:22:49 -0700

    lazy matrix multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -22,10 +22,10 @@ def lazy_matrix_mul(m_a, m_b):
         if not isinstance(row, list):
             raise TypeError('m_b must be a list of lists')
 
-    if (len(m_a) == 0):
+    if (len(m_a[0]) == 0):
         raise ValueError('m_a can\'t be empty')
 
-    if (len(m_b) == 0):
+    if (len(m_b[0]) == 0):
         raise ValueError('m_b can\'t be empty')
 
     for row in m_a:
