66a4821276f42ef98db5d708fddb2a093e40c122
Bayzman <bassmaninno102@gmail.com>
2024-04-14 19:07:24 -0700

    fixed coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,8 +39,8 @@ class Square:
         """ Decorator to set the position attribute
         """
         if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(isinstance(i, int) and i >= 0 for i in value)):
+                len(value) != 2 or
+                not all(isinstance(i, int) and i >= 0 for i in value)):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
