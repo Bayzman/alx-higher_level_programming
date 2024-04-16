f39af0b1c597d5ecc93578d0124f3ae1bcf69c0d
Bayzman <bassmaninno102@gmail.com>
2024-04-16 15:49:18 -0700

    read file

---
+++ 0x0B-python-input-output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Read a text file (UTF8)
+"""
+
+
+def read_file(filename="") -> None:
+    """ Reads a text file
+    """
+    with open(filename, "r") as f:
+        for line in f:
+           print(line, end="")
