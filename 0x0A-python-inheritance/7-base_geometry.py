2f8473590f43d82317b37179e259f3ebd105fd5f
Bayzman <bassmaninno102@gmail.com>
2024-04-15 19:41:54 -0700

    improved class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Create an Base Geometry class """
+
+
+class BaseGeometry:
+    """ Base Geometry """
+
+    def area(self) -> Exception:
+        """ Raise an exception """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value) -> Exception:
+        """ Integer validator """
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+
+        if (value <= 0):
+            raise ValueError(f'{name} must be greater than 0')
