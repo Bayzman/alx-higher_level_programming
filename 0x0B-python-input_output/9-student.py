c6c2a1d1277646d3a6e43c7ca7abda42ba820ddd
Bayzman <bassmaninno102@gmail.com>
2024-04-16 16:28:53 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" Class that defines a student """
+
+
+class Student:
+    """ Class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialization
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Retrieves dictionary representation
+        """
+
+        return self.__dict__
