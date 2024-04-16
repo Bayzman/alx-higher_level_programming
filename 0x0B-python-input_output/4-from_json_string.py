ebc760b0dfdb37c528a47b9fb0fa331698fbd188
Bayzman <bassmaninno102@gmail.com>
2024-04-16 16:05:36 -0700

    return an object (Python data structure)

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Returns an object (Python data structure) represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ Convert JSON to Python data structure (object)
+
+    Args:
+        my_str (str): JSON string representation
+
+    Return:
+        my_str (json): JSON string
+    """
+
+    return json.loads(my_str)
