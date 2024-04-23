c992151a6166224fe6132d6470e51b05cfd7c1f4
Bayzman <bassmaninno102@gmail.com>
2024-04-22 20:38:02 -0700

    added unit tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,41 +7,51 @@ from models.base import Base
 
 class TestBase_1st(unittest.TestCase):
     def setUp(self):
+        """ Setup method """
+        b = Base()
         print("Testing Base")
 
     def test_no_param_1_instance(self):
+        """ Test with no parameters """
         b = Base()
-        self.assertEqual(b.id, 2)
+        self.assertEqual(b.id, 6)
 
     def test_no_param_3_instances(self):
+        """ Test multiple instances """
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b2.id, 4)
+        self.assertEqual(b2.id, 9)
 
     def test_with_param(self):
+        """ Test with parameter """
         b1 = Base(3)
         self.assertEqual(b1.id, 3)
 
     def test_none_param(self):
+        """ Test with None """
         b1 = Base(None)
-        self.assertEqual(b1.id, 5)
+        self.assertEqual(b1.id, 11)
 
     def test_assign_new_id(self):
+        """ Assign new id """
         b2 = Base()
         b2.id = 6
         self.assertEqual(b2.id, 6)
 
     def test_bool_param(self):
+        """ Test with boolean parameter """
         b3 = Base(True)
         self.assertNotEqual(b3.id, False)
 
     def test_isisnstance(self):
+        """ Check for instances """
         b4 = Base(5)
         self.assertIsInstance(b4, Base)
 
     def test_privacy(self):
+        """ Check privacy of class object """
         with self.assertRaises(AttributeError):
             Base().__nb_objects
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -8,61 +8,76 @@ from models.base import Base
 
 class TestSquare_1st(unittest.TestCase):
     def setUp(self):
+        """ Setup method """
         print("Testing Square")
 
     def test_width(self):
+        """ Test width """
         s1 = Square(4, 5)
         self.assertEqual(s1.size, 4)
 
     def test_height(self):
+        """ Test height """
         s1 = Square(4, 5)
         self.assertEqual(s1.x, 5)
 
     def test_no_param(self):
+        """ Test no parameter """
         with self.assertRaises(TypeError):
             s2 = Square()
 
     def test_1_param(self):
+        """ Test 1 parameter """
         s3 = Square(3)
         self.assertEqual(s3.size, 3)
 
     def test_rect_from_base(self):
+        """ Check for instances """
         self.assertIsInstance(Square(4, 5), Base)
 
     def test_x(self):
+        """ Test x """
         s3 = Square(4)
         self.assertEqual(s3.x, 0)
 
     def test_x_2(self):
+        """ Test x again """
         s3 = Square(4, 5)
         self.assertEqual(s3.x, 5)
 
     def test_y(self):
+        """ Test y """
         s3 = Square(4, 5)
         self.assertEqual(s3.y, 0)
 
     def test_y_2(self):
+        """ Test y again """
         s3 = Square(4, 5, 7)
         self.assertEqual(s3.y, 7)
 
     def test_all_param(self):
+        """ Test all parameters """
         s3 = Square(4, 5, 7, 9)
         self.assertEqual(9, s3.id)
 
     def test_size_private(self):
+        """ Test for privacy """
         r2 = Rectangle(4, 5, 7, 8)
         with self.assertRaises(AttributeError):
             r2.__size
 
     def test_x_private(self):
+        """ Privacy test """
         r2 = Rectangle(4, 5, 7, 8)
         with self.assertRaises(AttributeError):
             r2.__x
 
     def test_y_private(self):
+        """ Privacy test """
         r2 = Rectangle(4, 5, 7, 8)
         with self.assertRaises(AttributeError):
             r2.__y
+
 
 if __name__ == '__main__':
     unittest.main()
