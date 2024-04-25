b08117cb73b86269a3dde6bf89bd073cebd124c0
Bayzman <bassmaninno102@gmail.com>
2024-04-24 18:14:49 -0700

    updated test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,30 +6,28 @@ from models.base import Base
 
 
 class TestBase_1st(unittest.TestCase):
-    def setUp(self):
-        """ Setup method """
-        self.b = Base()
-        self.b1 = Base()
-        self.b2 = Base()
-        print("Testing Base")
+    """ Test Base Module """
+    b1 = Base()
+    b2 = Base()
+    b3 = Base()
 
     def test_no_param_1_instance(self):
         """ Test with no parameters """
-        self.assertEqual(self.b.id, 1)
-
-    def test_no_param_3_instances(self):
-        """ Test multiple instances """
-        self.assertEqual(self.b2.id, 3)
+        self.assertEqual(self.b1.id, 1)
+        # b2.id should be 2 but because it has been assigned to 6
+        # in test_assign_new_id, b2.id != 2
+        self.assertEqual(self.b2.id, 6)
+        self.assertEqual(self.b3.id, 3)
 
     def test_with_param(self):
         """ Test with parameter """
-        self.b1 = Base(3)
-        self.assertEqual(self.b1.id, 3)
+        self.b4 = Base(3)
+        self.assertEqual(self.b4.id, 3)
 
     def test_none_param(self):
         """ Test with None """
-        self.b1 = Base(None)
-        self.assertEqual(b1.id, 11)
+        self.b5 = Base(None)
+        self.assertEqual(self.b5.id, 4)
 
     def test_assign_new_id(self):
         """ Assign new id """
@@ -38,13 +36,13 @@ class TestBase_1st(unittest.TestCase):
 
     def test_bool_param(self):
         """ Test with boolean parameter """
-        b3 = Base(True)
-        self.assertNotEqual(b3.id, False)
+        b6 = Base(True)
+        self.assertNotEqual(b6.id, False)
 
     def test_isisnstance(self):
         """ Check for instances """
-        b4 = Base(5)
-        self.assertIsInstance(b4, Base)
+        b5 = Base(5)
+        self.assertIsInstance(b5, Base)
 
     def test_privacy(self):
         """ Check privacy of class object """
