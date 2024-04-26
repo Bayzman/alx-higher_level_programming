e6e64cd09e06f09dd6f7ab37457c2cddcc6239ed
Bayzman <142792075+Bayzman@users.noreply.github.com>
2024-04-26 23:52:56 +0300

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -9,35 +9,42 @@ from models.base import Base
 class TestSquare(unittest.TestCase):
     """ Test cases """
     def setUp(self):
+        """ Setup test """
         self.square = Square(10, 1, 1, 1)
 
     def test_init(self):
+        """ Test constructor """
         self.assertEqual(self.square.size, 10)
         self.assertEqual(self.square.x, 1)
         self.assertEqual(self.square.y, 1)
         self.assertEqual(self.square.id, 1)
 
     def test_size_getter(self):
+        """ Test getter """
         self.assertEqual(self.square.size, 10)
 
     def test_size_setter(self):
+        """ Test setter """
         self.square.size = 5
         self.assertEqual(self.square.width, 5)
         self.assertEqual(self.square.height, 5)
 
     def test_update(self):
+        """ Test update """
         self.square.update(89, 2, 3)
         self.assertEqual(self.square.id, 89)
         self.assertEqual(self.square.x, 2)
         self.assertEqual(self.square.y, 3)
 
     def test_update_kwargs(self):
+        """ Test kwargs """
         self.square.update(size=5, x=4, y=5)
         self.assertEqual(self.square.size, 5)
         self.assertEqual(self.square.x, 4)
         self.assertEqual(self.square.y, 5)
 
     def test_to_dict(self):
+        """ Test to dictionary """
         to_dict = self.square.to_dictionary()
         self.assertIn('id', to_dict)
         self.assertIn('x', to_dict)
@@ -45,6 +52,7 @@ class TestSquare(unittest.TestCase):
         self.assertIn('y', to_dict)
 
     def test_str(self):
+        """ Test __str__ """
         self.assertEqual(str(self.square), '[Square] (1) 1/1 - 10')
 
     def test_x_private(self):
