bf04bffabd291a03a2049abde7c7a589b0231ac7
Bayman <bassmaninno102@gmail.com>
2024-06-19 06:48:31 -0700

    print multiple lines using a loop

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const firstArg = Number(process.argv[2]);
+
+if (firstArg) {
+  for (let i = 0; i < firstArg; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
