7c21d3ba3ba75d8a0e5221d8506d042e8f42c932
Bayzman <bassmaninno102@gmail.com>
2024-07-06 04:14:36 -0700

    fixed a bug

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 
 """ takes in a URL and an email, sends a POST request """
-
 from sys import argv
-from urllib import request, parse
+import urllib.parse
+from urllib.request import Request, urlopen
 
 
 def main():
-    """ Main function
-    """
+    """ Main function """
     url = argv[1]
     email = argv[2]
     values = {'email': email}
-    content = parse.urlencode(values)
-
-    request = request.Request(url, content.encode('ascii'))
-    with request.urlopen(request) as response:
+    content = urllib.parse.urlencode(values)
+    req = Request(url, content.encode('ascii'))
+    with urlopen(req) as response:
         html = response.read()
         print(html.decode('utf-8'))
 
